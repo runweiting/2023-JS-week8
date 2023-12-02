bedd4724b4cb60100798f8cbb2b3deb5b138f9e1
runweiting <whatisfuntime@gmail.com>
2023-12-02 23:12:05 +0800

    [feat]: 完成前台GET,POST,PATCH,DELETE API

---
+++ assets/js/admin.js
@@ -0,0 +1,133 @@
+/* 共用 ------- */
+const config = {
+    headers: {
+        'Authorization':'CXLvMgR3dAaogAeoJqJ2IhKaL4P2'
+    }
+};
+
+/* 後台 url ------- */
+const adminUrl = 'https://livejs-api.hexschool.io/api/livejs/v1/admin/runweiting';
+const ordersUrl = `${adminUrl}/orders`;
+
+let orderData = [];
+
+// 1：GET 取得訂單列表 getOrderList()
+function getOrderList(){
+    axios
+    .get(ordersUrl,config)
+    // -> 
+    .then((res)=>{
+        orderData = res.data.orders;
+        renderOrder(orderData);
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+};
+// 渲染訂單列表 -> renderOrder() 
+function renderOrder(array){
+    const orderItem = document.querySelector('#orderItem');
+    if (!orderItem) {
+        console.error('找不到 #orderItem 元素');
+        return;
+    };
+    let str = '';
+    array.forEach((item) => {
+        str += `
+        <tr class="text-nowrap">
+        <td>${item.id}</td>
+        <td>
+            <p>${item.user.name}</p>
+            <p>${item.user.tel}</p>
+        </td>
+        <td>${item.user.address}</td>
+        <td>${item.user.email}</td>
+        <td>
+            <p>${item.products[0].title}</p>
+        </td>
+        <td>${new Date(item.createdAt * 1000).toLocaleDateString()}</td>
+        <td class="orderStatus">
+            <a href="#" class="statusBtn" data-id="${item.id}">${item.paid ? '已處理' : '未處理'}</a>
+        </td>
+        <td>
+            <input type="button" class="removeBtn" data-id="${item.id}" value="刪除">
+        </td>
+        </tr>`;      
+    });
+    orderItem.innerHTML = str;
+    // -> 在每次渲染後，重新設定按鈕監聽
+    const statusBtn = document.querySelectorAll('.statusBtn');
+    statusBtn.forEach((item)=>{
+        item.addEventListener('click',changeOrderStatus);
+    });
+    const removeBtn = document.querySelectorAll('.removeBtn');
+    removeBtn.forEach((item)=>{
+        item.addEventListener('click',removeOrderItem);
+    });
+    const removeAllBtn = document.querySelectorAll('.btn-warning');
+    removeAllBtn.forEach((item)=>{
+        item.addEventListener('click',removeAllorders);
+    })
+};
+
+// 2. PUT 修改訂單狀態 changeOrderStatus()
+function changeOrderStatus(e){
+    e.preventDefault();
+    const orderId = e.target.dataset.id;
+    axios
+    .put(ordersUrl,
+        {
+            "data": {
+                "id": orderId,
+                "paid": true
+            }
+        },
+    config)
+    // ->
+    .then((putRes)=>{
+        console.log(putRes.data);
+        alert(`訂單編號：${orderId} 已處理`);
+        renderOrderStatus(orderId,putRes.data.paid);
+        getOrderList();
+    })
+    .catch((putErr)=>{
+        console.log(putErr)
+    })
+};
+// 渲染訂單狀態 -> renderOrderStatus()
+function renderOrderStatus(orderId,paid){
+    const statusBtn = document.querySelector(`.statusBtn[data-id="${orderId}"]`);
+    statusBtn.textContent = paid ? "已處理" : "未處理"
+};
+
+// 3. DELETE 刪除特定訂單 -> removeOrderItem() 
+function removeOrderItem(e){
+    const deleteOrderUrl = `${ordersUrl}/${e.target.dataset.id}`;
+    axios
+    .delete(deleteOrderUrl,config)
+    .then((delRes)=>{
+        alert(`訂單編號：${e.target.dataset.id} 已刪除`);
+        getOrderList();
+        console.log(delRes.data);
+    })
+    .catch((delErr)=>{
+        alert('請稍後再試');
+        console.log(delErr)
+    })
+};
+
+// 4. DELETE 刪除全部訂單 -> removeAllorders()
+function removeAllorders(e){
+    axios
+    .delete(ordersUrl,config)
+    .then((delRes)=>{
+        console.log(delRes.data);
+        alert('成功刪除全部訂單');
+        getOrderList();
+    })
+    .catch((delErr)=>{
+        console.log(delErr)
+    })
+};
+
+getOrderList();
---
+++ assets/js/index.js
@@ -0,0 +1,204 @@
+/* 共用 ------- */
+const config = {
+    headers: {
+        'Authorization':'CXLvMgR3dAaogAeoJqJ2IhKaL4P2'
+    }
+};
+
+/* 前台 url ------- */
+const customerUrl = 'https://livejs-api.hexschool.io/api/livejs/v1/customer/runweiting';
+const productUrl = `${customerUrl}/products`;
+const cartUrl = `${customerUrl}/carts`;
+
+let filterProductData = [];
+let productData = [];
+let cartData = [];
+
+// 1-1. GET 取得產品列表 -> getProductList() 
+function getProductList(){
+    axios
+    .get(productUrl)
+    .then((res)=>{
+        productData = res.data.products;
+        renderProduct();
+        console.log(res.data)
+    })
+    // -> 加入監聽
+    .then(()=>{
+        
+        //renderProduct();
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+};
+getProductList();
+// 2-1. GET 取得購物車列表 -> getCartList()
+function getCartList(){
+    axios
+    .get(cartUrl)
+    .then((res)=>{
+        cartData = res.data.carts;
+        let cartFinalTotal = res.data.total;
+        renderCartList(cartData,cartFinalTotal);
+        console.log(res.data)
+    })
+    // ->
+    .catch((err)=>{
+        console.log(err)
+    })
+};
+getCartList();
+
+// 1-2. 渲染、篩選產品列表 -> renderProductList()
+function renderProduct(category = "全部商品"){
+    filterProductData = category === "全部商品" ?
+    productData : productData.filter(item =>
+        item.category === category);
+    const searchResult = document.querySelector('.searchResult');
+    searchResult.textContent = filterProductData.length? `本次搜尋共${filterProductData.length}筆資料` : `查無此關鍵字資料`;
+    const productList = document.querySelector('.productList');
+    let str = '';
+    filterProductData.forEach((item)=>{
+        str += `
+        <li class="col-3 h-100 position-relative">
+            <span class="bg-dark text-white fs-5 px-6 py-2 position-absolute" style="top: 12px;right: 8px;">新品</span>
+            <img src="${item.images}" class="card-img-top" alt="產品圖片-${item.title}">
+            <input type="button" value="加入購物車" class="bg-dark fs-5 text-white addCartItemBtn w-100 py-2" data-id="${item.id}">
+            <div class="card-body">
+            <h5 class="card-title fs-5 py-2">${item.title}</h5>
+            <p class="card-text fs-5"><del>${item.origin_price}</del></p>
+            <p class="card-text fs-3">${item.price}</p>
+            <input type="hidden" class="quantityInput" value="1">
+            </div>
+        </li>`;
+    });
+    productList.innerHTML = str;
+    // -> 在每次渲染後，重新設定按鈕監聽
+    const categorySearch = document.querySelector('#categorySearch');
+    categorySearch.addEventListener('change',(e)=>{
+        renderProduct(e.target.value);
+    });
+    const addCartItemBtn = document.querySelectorAll('.addCartItemBtn');
+    addCartItemBtn.forEach((item)=>{
+        item.addEventListener('click',(e)=>{
+            const targetID = e.target.dataset.id;
+            postCartItem(targetID);
+        })
+    });
+};
+
+// 2-2. 渲染購物車列表 -> renderCartList()
+function renderCartList(array,total){
+    const cartItem = document.querySelector('#cartItem');
+    const cartFinalTotal = document.querySelector('#cartFinalTotal');
+    let str = '';
+    array.forEach((item)=>{
+        str += `
+        <tr class="text-nowrap fs-5">
+        <td class="d-flex align-items-center py-5">
+          <div class="overflow-hidden" style="height: 80px;">
+            <img src="${item.product.images}" class="me-4" alt="產品圖片-${item.product.title}" style="width: 80px">
+          </div>
+          <p>${item.product.title}</p>
+        </td>
+        <td class="py-5">${item.product.price}</td>
+        <td class="py-5">${item.quantity}</td>
+        <td class="py-5">${(item.product.price)*(item.quantity)}</td>
+        <td class="py-5">
+          <a href="#" class="removeBtn d-flex justify-content-center align-items-center text-decoration-none" data-id="${item.id}">
+            <span class="material-symbols-outlined text-dark" style="font-size: 24px;">
+              close
+            </span>
+          </a>
+        </td>
+      </tr>`;
+    });
+    cartItem.innerHTML = str;
+    // 使用 Intl.NumberFormat 來格式化數字，再 formatter.format(number) 將數字格式化為字串
+    cartFinalTotal.textContent = `NT$ ${total.toLocaleString('en-US')}`;
+    // -> 在每次渲染後，重新設定按鈕監聽
+    const removeBtn = document.querySelectorAll('.removeBtn');
+    removeBtn.forEach((item)=>{
+        item.addEventListener('click',removeCartItem);
+    });
+};
+
+// 3. POST 加入購物車 -> postCartItem()
+// note: 先 GET 檢查是否有相同商品，有 PATCH +1，無 POST 1
+function postCartItem(targetID){
+    const postData = {
+        "data": {
+          "productId": targetID,
+          "quantity": 1
+        }
+      };
+    // 先檢查是否有相同商品
+    axios
+    .get(cartUrl)
+    .then((res)=>{
+        let cartListData = res.data.carts;
+        const existingCartItem = cartListData.find(item=> item.product.id === targetID);
+        if (existingCartItem){
+            // 有 PATCH +1
+            axios
+            .patch(cartUrl,{
+                "data": {
+                  "id": existingCartItem.id,
+                  "quantity": existingCartItem.quantity + 1
+                }
+              })
+            .then((patchRes)=>{
+                alert(`${existingCartItem.product.title} 數量 +1`);
+                getCartList();
+                console.log(patchRes.data);
+            })
+            .catch((patchErr)=>{
+                alert('請稍後再試');
+                console.log(patchErr)
+            })
+        } else {
+            // 無 POST 1
+            axios
+            .post(cartUrl,postData)
+            .then((postRes)=>{
+                alert('已成功加入購物車！');
+                getCartList();
+                console.log(postRes.data.carts)
+            })
+            .catch((postErr)=>{
+                alert('請稍後再試');
+                console.log(postErr)
+            })
+        };
+        console.log(res.data);
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+};
+
+// 4. DELETE 刪除購物車特定商品 -> removeCartItem()
+function removeCartItem(e){
+    e.preventDefault();
+    const targetID = e.currentTarget.dataset.id;
+    const deleteCartItemUrl = `${cartUrl}/${targetID}`;
+    axios
+    .delete(deleteCartItemUrl,config)
+    .then((delRes)=>{
+        alert(`購物車編號：${targetID} 已刪除`);
+        getProductList();
+        getCartList();
+        console.log(delRes.data);
+    })
+    .catch((delErr)=>{
+        alert('請稍後再試');
+        console.log(delErr);
+    })
+};
+
+// 5. DELETE 刪除購物車 -> removeCart()
+
+
+
+
